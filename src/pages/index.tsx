8b2bfdbc27ca98c1e353b2fb9a411d6f6820aa58
LucasFlaquer <lucas.flaquer@gmail.com>
2021-11-25 22:52:29 -0300

    refactoring home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,42 +1,62 @@
-import type { NextPage } from 'next'
-
 import styled from 'styled-components';
 
 export const Container = styled.div`
-  max-width: 1200px;
+  max-width: 1200px; 
   margin: 0 auto;
   height: 100vh;
   padding-top: 45px;
 `;
-
 export const Title = styled.h1`
-  background: -webkit-linear-gradient(#EF6C00, #DB6300);
+  background: -webkit-linear-gradient(#ef6c00, #db6300);
+  background-clip: text; 
   -webkit-background-clip: text;
   -webkit-text-fill-color: transparent;
   text-transform: uppercase;
   font-size: 36px;
   text-align: center;
-`
+`;
 export const Separator = styled.span`
   height: 1px;
-  background: #CFCFCF;
+  background: #cfcfcf;
   width: 255px;
-  display: block;
+  display: block; 
   margin: 12px auto 0;
-`
+`;
+export const Content = styled.div`
+  display: flex;
+  justify-content: center;
+  margin-top: 130px;
+`;
+export const Digit = styled.div`
+  height: 200px;
+  width: 100px;
+  background: #353131;
+  position: relative;
 
+  .top {
+    img {
+      fill: red;
+    }
+  }
+`;
 
-
-const Home: NextPage = () => {
+export default function Home(): JSX.Element {
   return (
     <Container>
-      <Title>
+      <Title> 
         Qual é o Número?
         <Separator />
-      </Title>
-
+      </Title> 
+      <Content>
+        <Digit>
+          <span />
+          <span />
+          <span />
+          <span />
+          <span />
+          <span />
+        </Digit>
+      </Content>
     </Container>
-  )
+  );
 }
-
-export default Home
