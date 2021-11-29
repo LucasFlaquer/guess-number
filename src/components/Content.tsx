2f6c001d40c247f0d1a9d33630ba785f92e3aecf
LucasFlaquer <lucas.flaquer@gmail.com>
2021-11-29 19:39:14 -0300

    content and guess number

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -3,6 +3,7 @@ import styled from "styled-components";
 export const Content = styled.div`
   display: flex;
   justify-content: center;
+  flex-direction: column;
+  align-items: center;
   margin-top: 130px;
-  
 `;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -26,21 +26,103 @@ export const DigitsList = styled.div`
       margin-left: 15px;
     }
   }
-  
 `; 
 
+export const Form = styled.form`
+  margin-top: 40px;
+  display: flex;
+`;
+
+export const Input = styled.input`
+  padding: 14px 8px;
+  border: 1px solid #CFCFCF;
+  border-radius: 4px;
+  font-family: 'Roboto', sans-serif;
+  font-weight: 400;
+  font-size: 12px;
+  height: 42px;
+  min-width: 210px;
+  margin-right: 15px;
+`;
+
+export const Button = styled.button`
+  display: block;
+  height: 42px;
+  text-transform: uppercase;
+  background: linear-gradient(#ef6c00, #db6300);
+  padding: 14px 8px;
+  border-radius: 4px;
+  color: #fff;
+  font-family: 'Roboto', sans-serif;
+  font-weight: bold;
+  border: 1px solid transparent;
+  transition: filter .2s ease-in-out;
+
+  &:hover {
+    filter: brightness(0.8);
+  }
+`;
+
+interface MessageProps {
+  messageColor: string
+}
+
+export const Message = styled.p<MessageProps>`
+  text-align: center;
+  text-transform: uppercase;
+  font-weight: bold;
+  height: 15px;
+  margin-bottom: 40px;
+  color: ${props => props.messageColor || '#FF6600'};
+
+`;
+
 export default function Home({value, error}:HomeProps): JSX.Element {
-  const [number, setNumber] = useState(0);
+  const [guessNumber, setGuessNumber] = useState('');
   const [digits, setDigits] = useState<ColorSchema[]>([]);
   const [correctNumber, setCorrectNumber] = useState(0);
-  const digit = numberColorsSchema(9);
   const [message, setMessage] = useState('');
+  const [digitColor, setDigitColor] = useState('#262A34');
+  const [messageColor, setMessageColor] = useState('');
+  const [isDisabled, setIsDisabled] = useState(false);
   useEffect(()=> {
-    setDigits([numberColorsSchema(2), numberColorsSchema(3)])
+    setDigits([numberColorsSchema(0)])
+    setCorrectNumber(value);
+    if(error) {
+      setMessage('ERRO');
+      setMessageColor('#CC3300');
+      setDigitColor('#CC3300');
+      setDigits([numberColorsSchema(5), numberColorsSchema(2), numberColorsSchema(2)]);
+    } 
   }, [])
+
   async function handleSubmit(event: React.FormEvent<HTMLFormElement>): Promise<void> {
     event.preventDefault();
+    if(guessNumber === '') return;
+    const form = event.target as HTMLFormElement;
     
+    console.log(guessNumber, correctNumber)
+    if(Number(guessNumber) === correctNumber) {
+      setMessage('Você acertou!!!!');
+      setMessageColor('#00CC33');
+      setDigitColor('#00CC33');
+      setIsDisabled(true);
+    }
+    else if(Number(guessNumber) > correctNumber) 
+      setMessage('É maior');
+    else setMessage('É menor');    
+    const digits = guessNumber.split('').map(number => numberColorsSchema(Number(number)));
+    setDigits(digits);  
+  }
+  function handleInputChange(event: React.ChangeEvent<HTMLInputElement>): void {
+    console.log(event.target.value)
+    if(event.target.value === '') {
+      setGuessNumber('');
+      return;
+    }
+    if(event.target.value.match(/^\d+$/)) {
+      setGuessNumber(event.target.value);
+    }
   }
 
   return (
@@ -50,12 +132,13 @@ export default function Home({value, error}:HomeProps): JSX.Element {
         <Separator />
       </Title>
       <Content>
+        <Message messageColor={messageColor}>{message}</Message>
         <DigitsList>
           {digits.map((digit, index) => (
             <Digit 
               height={100 * 2}
               width={56 * 2}
-              trueColor={'#262A34'}
+              trueColor={digitColor}
               topLed={digit.top}
               topLeftLed={digit.topLeft}
               topRightLed={digit.topRight}
@@ -63,10 +146,22 @@ export default function Home({value, error}:HomeProps): JSX.Element {
               bottomLeftLed={digit.bottomLeft}
               bottomRightLed={digit.bottomRight}
               middleLed={digit.middle}
+              key={index}
             />
           ))}
         </DigitsList>
-        <p>{message}</p>
+        
+        
+
+        <Form onSubmit={handleSubmit}>
+          <Input 
+            placeholder="Digite o palpite"
+            type="text"
+            value={guessNumber}
+            onChange={handleInputChange} 
+            disabled={isDisabled}/>
+          <Button disabled={isDisabled} type="submit">Enviar</Button>
+        </Form>
       </Content>
     </Container>
   );
@@ -75,6 +170,7 @@ export default function Home({value, error}:HomeProps): JSX.Element {
 export const getStaticProps: GetStaticProps = async () => {
   try {
     const number = await fetchNumber();
+    console.log(number)
     return {
       props: {
         value: number.value
